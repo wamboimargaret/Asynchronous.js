a30757c6863c0530d816009eb423d332e4ca7cbe
student <margarettwmboi@gmail.com>
2023-05-09 10:00:31 +0300

    async

--- async.js
+++ async.js
@@ -1,5 +1,10 @@
 // Write an asynchronous function that accepts a message string and a delay time in milliseconds. 
 //The function should log the message to the console after the specified delay time.
+let delayMessage = () => {
+    console.log('I will come late');
+};
+setTimeout(delayMessage,3000);
+
 
 // You have an array of user IDs and a function getUserData(id) that returns a Promise with user 
 //data when given a user ID. Write an asynchronous function that fetches and logs the data for 
@@ -10,3 +15,22 @@
  //the task is successful and rejects if there's an error. Write a function that calls performTask()
   //and logs a custom success message if the task is successful, and a custom error message if there's 
   //an error.
+  function call(num, callback){
+  console.log({callback});
+  }
+
+  let task = true;
+  function performTask(){
+   let promise = new Promise(function(resolve,reject){
+    if(task){
+        resolve('The task was succesful');
+    }
+    else{
+        reject('The task was not succesful')
+    }
+   });
+   return promise
+  }
+  
+
+call(10,performTask);
